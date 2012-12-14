963f05e817d59220f7af5088e3864ddd1b68ba54
BurnSpirit <burnspirit@gmail.com>
2012-12-14 11:48:40 -0500

    grafico combustivel draggable

--- graficos/gasto_combustivel.php
+++ graficos/gasto_combustivel.php
@@ -4,6 +4,7 @@
     *
     */
 ?>
+    <h3>Analise de Viaturas</h3>
     <style>
         #drag_viatura { width:100px; height: 100px; padding: 0.5em; float: left; margin: 10px 10px 10px 0;};
         #caixa_viaturas {width:150px; height: 150px; padding: 0.5em; float: left; margin: 10px;}
