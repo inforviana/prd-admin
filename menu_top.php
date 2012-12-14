de9cdc3cd91e734464316252e623af23458135b3
BurnSpirit <burnspirit@gmail.com>
2012-12-14 13:32:58 -0500

    data de trabalho para as listagens

--- menu_top.php
+++ menu_top.php
@@ -38,7 +38,7 @@
 								<li><a href="#">Horas por Viatura</a></li>
 							</ul>	
 						</li>
-						<li><a href="index.php"><span>Opções</span></a>
+						<li><a href="index.php"><span>Opï¿½ï¿½es</span></a>
 							<ul class="sub_menu">
 								<li><a href="index.php?pagina=opcoes">Parametros Gerais</a></li>
 								<li><a href="index.php?pagina=utilizadores">Utilizadores</a></li>
@@ -51,6 +51,34 @@
 			<br><br>
 			<font style="font-family:Arial, Helvetica, sans-serif;font-size:11px;">'.$NOME_APP.' - '.$VERSAO_APP.'</font>					
 		</div>
-
+        <br>
 		';
+           
+    //seleccao da data de trabalho
+    if(isset($_COOKIE['data_i']))
+    {
+        $data_i=$_COOKIE['data_i'];
+        $data_f=$_COOKIE['data_f'];
+    }else{
+        $data_i=date('Y-m-d',strtotime('-1 month'));
+        $data_f=date('Y-m-d'); 
+    }
+    
+    echo '
+        <script>
+            $(function(){
+                $("#data_i, #data_f").datepicker({dateFormat: "yy-mm-dd"});
+            });
+        </script>
+        
+        <form method="POST" action="./index.php?a=datatrabalho">
+            <table>
+                <tr>
+                    <td><input name="data_i" id="data_i" type="text" placeholder="data inicial" style="width:150px;text-align:center;font-size:18px;" value="'.$data_i.'"></td>
+                    <td><input name="data_f" id="data_f" type="text" placeholder="data final" style="width:150px;text-align:center;font-size:18px;" value="'.$data_f.'"></td>
+                    <td><input type="submit" value="OK"></td>
+                </tr>
+            </table>
+        </form>
+        ';
 ?>
