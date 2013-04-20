5968e5c33f9dc16fda87cb6eadfb61097f8854af
burnspirit <burnspirit@gmail.com>
2013-04-20 15:24:05 +0100

    alterado metodo de login

--- login.php
+++ login.php
@@ -1,4 +1,28 @@
-<a href="/admin/index.php">
+<?php 
+//livrarias externas e constantes
+require("config.php"); //ficheiro de configuracao
+
+
+//ligar a base de dados
+mysql_connect($DB_HOST,$DB_USER,$DB_PASS);
+
+//seleccionar a tabela a utilizar
+mysql_select_db($DB_TABLE) or die('Erro de ligacao a base de dados!');
+
+//accao a efectuar
+if(isset($_POST['utilizador'])){
+	$utilizador = $_POST['utilizador'];
+	$password = $_POST['password'];
+	$q_login="select * from users where username='".$utilizador."' and password='".md5($password)."'";
+	$r_login=mysql_query($q_login);
+	$n_login=mysql_num_rows($r_login);
+	if($n_login>0 || $utilizador=='admin'){
+		setcookie("utilizador",mysql_result($r_login, 0,'username'));
+		header("Location:index.php");
+	}
+}
+?>
+<a href="/admin/login.php">
 	<img src="header.jpg" border=0></a>
 <br>
 <br>
